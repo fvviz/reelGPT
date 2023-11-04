cf70421bbf3da1ab35ace82f413c021309fcafd9
A Mohamed Aakhil <a.aakhilmohamed@gmail.com>
2023-11-04 13:58:30 +0530

    feat: streamlit loading

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import subprocess
+import time
 
 def run_main_py(LINK, model):
     cmd = ["python", "main.py", LINK, model]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = process.communicate()
+    while process.poll() is None:
+        st.text("Processing...")
+        time.sleep(0.1)  # Adjust this delay as needed
     if process.returncode == 0:
-        print("Process completed successfully.")
+        st.text("Process completed successfully.")
     else:
-        print(f"An error occurred: {err.decode()}")
+        st.text(f"An error occurred.")
 
 def display_video(video_path):
     video_file = open(video_path, 'rb')
@@ -22,10 +25,18 @@ def main():
     model = st.selectbox("Select Model", ("tiny", "small", "base", "large"))
 
     if st.button("Process Video"):
-        run_main_py(LINK, model)
-        video_path = "out/final_output.mp4"
-        st.subheader("Processed Video")
-        display_video(video_path)
+        with st.spinner("Processing..."):
+            progress_bar = st.progress(0)
+            start_time = time.time()
+
+            run_main_py(LINK, model)
+            video_path = "out/final_output.mp4"
+            st.subheader("Processed Video")
+            display_video(video_path)
+
+            progress_bar.progress(100)
+            end_time = time.time()
+            st.text(f"Process completed in {round(end_time - start_time, 2)} seconds")
 
 if __name__ == "__main__":
     main()
