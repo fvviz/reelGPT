2d8beece15d58f8999aac7e3941d28e95737dfbf
BootesVoid <78485654+AMohamedAakhil@users.noreply.github.com>
2023-11-04 19:33:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,45 +5,35 @@ from utils.llm_process import ReelSelection
 from utils.subtitle_overlay import SubtitleOverlay
 import os
 
-parser = argparse.ArgumentParser(description="Process a YouTube link and generate a transcribed video clip.")
+def start(LINK, MODEL):
 
-parser.add_argument("LINK", help="YouTube video link")
-parser.add_argument("model", help="Transcription model (default is 'tiny')", default="tiny")
+    print(f"Extracting link: {LINK} using model: {MODEL}")
 
-args = parser.parse_args()
+    ev = ExtractVideo(directory=".")
+    audio = ev.extract_youtube_audio(LINK)
 
+    print(f"Extracted video and audio")
 
-LINK = args.LINK
-MODEL = args.model
+    transcription_model = TranscriptionModel(audio, MODEL)
+    srt_out = transcription_model.transcribe_file()
+    rs = ReelSelection(srt_out)
+    out = rs.get_imp_parts()
 
-print(f"Extracting link: {LINK} using model: {MODEL}")
+    print(f"Extracted transcription and important parts")
 
-ev = ExtractVideo(directory=".")
-audio = ev.extract_youtube_audio(LINK)
+    vid_path = ev.extract_video(LINK)
 
-print(f"Extracted video and audio")
+    sub_overlay = SubtitleOverlay(vid_path, srt_out) 
+    sub_overlay.overlay_subtitle()
+    print("Overlayed subtitles")
 
-transcription_model = TranscriptionModel(audio, MODEL)
-srt_out = transcription_model.transcribe_file()
-rs = ReelSelection(srt_out)
-out = rs.get_imp_parts()
+    st = timestamp_to_seconds(out['start_time'])
+    et = timestamp_to_seconds(out['end_time'])
 
-print(f"Extracted transcription and important parts")
+    print("Extracted timestamps")
 
-vid_path = ev.extract_video(LINK)
+    clip_video(st, et, "final_output.mp4", "out/final_output.mp4")
 
-sub_overlay = SubtitleOverlay(vid_path, srt_out) 
-sub_overlay.overlay_subtitle()
-print("Overlayed subtitles")
-
-st = timestamp_to_seconds(out['start_time'])
-et = timestamp_to_seconds(out['end_time'])
-
-print("Extracted timestamps")
-
-clip_video(st, et, "final_output.mp4", "out/final_output.mp4")
-
-print("Clipped Video")
-
+    print("Clipped Video")
 
 
